dddbac6567f643445bcb123a577185beb85e4df5
sebaortega <ortegamolarosebastian@gmail.com>
2022-07-05 18:00:10 -0300

    preEntregaFinal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,34 +4,27 @@ import CarritoItem from "./CartItem";
 import { getFirestore,addDoc, collection } from "firebase/firestore";
 
 const Carrito = () => {
-  const { carrito , VaciarCarrito, PrecioTotal,CarritoItem, IconCarrito } = useContext( CarritoContext);
+  
+  const { carrito , VaciarCarrito, PrecioTotal, IconCarrito } = useContext( CarritoContext);
+  
   const generarOrden =()=>{
 
     const db = getFirestore()
-    const queryCollection = collection(db, "ordenes")   
+    const queryCollection = collection(db, "ordenes") 
+    const total = PrecioTotal()  
     const comprador = {nombre: 'seba', numero:'155333111', email: 'sebastian.ortega@hotmail.com'}
-    const orden = {comprador, carrito,PrecioTotal}
+    const orden = {comprador, carrito,total}
 
     const pedido = addDoc(queryCollection, orden)
-
-    pedido
-      .then((resp=>{
-          alert("usted ha comprado con exito: " + resp.id)
-      })
-      .catch((error=>{
-        console.log(error)
-      })
-      .finally((resp)=>{
-        VaciarCarrito();
-      })
-
-        
-   
+    pedido.then((resp)=>{
+      alert("usted ha comprado con exito: " + resp.id)})
+      .catch((error)=>{console.log(error)})
+      .finally((resp)=>{VaciarCarrito()})
   }
     
   return (
     <>
-    <div >
+    <div>
     {carrito.length < 1 ? (
         <p>Carrito vacio</p>
     ) : (
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -3,11 +3,12 @@ import{ CarritoContext} from "../contexts/CartContext";
 
 const CarritoItem = ({producto}) => {
     const {delItem} = useContext( CarritoContext);
-    const {nombre, precio,id}= producto;
+    const {nombre, precio,id,pictureUrl}= producto;
     return (
-    <div>
-        <h4>{nombre}</h4>
-        <h4>{precio}</h4>
+    <div style={{margin:("30px","50px","50px","50px") }} className="col-md6" >
+        <h4>Producto = {nombre}</h4>
+        <h4>{pictureUrl}</h4>
+        <h4>Precio = {precio}</h4>
         <button onClick={()=> delItem(id)}>borrar item</button>
     </div>
     )
